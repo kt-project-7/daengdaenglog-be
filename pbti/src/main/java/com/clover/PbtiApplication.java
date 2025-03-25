b56a300ae5ccdb3ac8d8baeaa77f07a9e4387cb3
leedy3838 <leedy3838@naver.com>
2025-03-25 15:03:13 +0900

    init: repository 생성

---
+++ pbti/src/main/java/com/clover/PbtiApplication.java
@@ -0,0 +1,13 @@
+package com.clover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbtiApplication.class, args);
+	}
+
+}
