8f9b11ad5f8442d2b06e720f71ecd8f208228100
leedy3838 <leedy3838@naver.com>
2025-03-31 10:01:05 +0900

    feat: ai 서비스 분리

---
+++ ai/src/main/java/com/clover/controller/AiController.java
@@ -0,0 +1,23 @@
+package com.clover.controller;
+
+import com.clover.service.AiService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "AiController", description = "Ai 관련 API")
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping
+@RestController
+public class AiController {
+
+    private final AiService aiService;
+
+    //TODO: 추억 그림 생성 api - ai를 이용한 추억 그림 생성 및 memory 테이블에 추가
+    @GetMapping("/test")
+    public String test() {
+        return aiService.generateImage("asd");
+    }
+}
